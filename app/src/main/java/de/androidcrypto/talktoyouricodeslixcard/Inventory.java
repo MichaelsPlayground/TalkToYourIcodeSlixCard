8cf6f87cba8105981185fc81167e6355b9db62cf
FlutterCrypto <fluttercrypto@gmx.de>
2023-11-19 12:21:44 +0100

    updated

--- app/src/main/java/de/androidcrypto/talktoyouricodeslixcard/Inventory.java
+++ app/src/main/java/de/androidcrypto/talktoyouricodeslixcard/Inventory.java
@@ -12,8 +12,9 @@ public class Inventory {
     private byte uid6IcManufacturerCode;
     private byte uid5TagType;
     private byte[] uid4SerialNumber;
-    private boolean isIcodeFamily;
+    private boolean isIcodeFamily = false;
     private boolean isManufacturerNxp;
+    private String manufacturerName = "unknown";
 
     private boolean isValid = false;
     public Inventory(byte[] response) {
@@ -24,7 +25,21 @@ public class Inventory {
         }
         data = response[0];
         uid = Arrays.copyOfRange(response, 1, 8);
-        //
-
+        // analyze data
+        analyzeData();
     }
+
+    private void analyzeData() {
+        uid7 = uid[0];
+        if (uid7 == (byte) 0xe0) isIcodeFamily = true;
+        uid6IcManufacturerCode = uid[1];
+        // todo lookup table
+        if (uid6IcManufacturerCode == (byte) 0x04) {
+            isManufacturerNxp = true;
+            manufacturerName = "NXP";
+        }
+        uid5TagType = uid[2];
+        // todo tag type lookup table
+    }
+
 }
